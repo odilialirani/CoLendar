7163576dd70057d5f8aad8f5e5e49bc20bc22a06
Odilia Lirani <oclirani@gmail.com>
2016-09-30 22:25:52 -0400

    Created server

---
+++ server.js
@@ -0,0 +1,11 @@
+// Server for Colendar
+// Created by Odilia Lirani
+
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+
+app.listen(PORT, () => {
+	console.log("Listening...");
+});
